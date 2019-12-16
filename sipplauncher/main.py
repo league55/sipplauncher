7f45753a64aba002a836e85b7bf719d5fe8d1535
Andrii Arsirii <pcapdump@gmail.com>
2019-12-16 05:42:54 -0500

    upd

--- sipplauncher/main.py
+++ sipplauncher/main.py
@@ -15,6 +15,7 @@ import sys
 import inspect
 from .utils.Signals import SignalException, capture_all_signals, check_signal
 from .utils.Utils import is_tls_transport
+import sslkeylog
 
 # import warnings
 # warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -52,13 +53,7 @@ def my_main_fun():
         We preload libsslkeylog.so to patch openssl.so to intercept and dump keys.
         """
         if is_tls_transport(args.sipp_transport):
-            app_dir = os.path.dirname(inspect.getfile(sipplauncher))
-            libsslkeylog_so_path = os.path.join(app_dir, "../lib", "libsslkeylog.so")
-            if os.path.isfile(libsslkeylog_so_path):
-                os.environ["SSLKEYLOGFILE"] = "tls_premaster_keys.txt"
-                os.environ["LD_PRELOAD"] = libsslkeylog_so_path
-            else:
-                raise Exception("{0} not found. Please verify it is installed".format(libsslkeylog_so_path))
+            sslkeylog.set_keylog("sslkeylog.txt")
 
     # Init ret code - pesimistic
     ret_code = 1
