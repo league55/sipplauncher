35e7573b2f8375f647f8a1011013bd51dff14be5
Andrii Arsirii <pcapdump@gmail.com>
2019-12-17 10:06:42 -0500

    similify

--- test/test_sipplauncher_Test_chdir.py
+++ test/test_sipplauncher_Test_chdir.py
@@ -9,12 +9,10 @@
 
 import pytest
 import tempfile
-import sys
 import os
 import shutil
 import shlex
 import sipplauncher.utils.Utils
-import sipplauncher.Network
 import logging
 
 from pytest_mock import mocker
@@ -22,12 +20,7 @@ from sipplauncher.utils.Utils import gen_file_struct
 from sipplauncher.utils.Init import (generate_parser,
                                      check_and_patch_args)
 from sipplauncher.Test import SIPpTest, STATES
-from sipplauncher.utils.Defaults import DEFAULT_TESTSUITE_TEMPLATES
 
-# Currently we are unit-testing keyword replacement for 2 UAs max.
-# Therefore we define 2 IP address for tests.
-# If you need to test more UAs - feel free to extend the list.
-UA_IP = ["10.22.22.100", "10.22.22.101"]
 DUT_IP = "1.1.1.1"
 TEST_NAME = "my_test_name"
 TEST_RUN_ID = sipplauncher.utils.Utils.generate_id(n=6, just_letters=True)
@@ -62,7 +55,6 @@ def test(mocker, mock_fs, args, expected):
     dirpath = tempfile.mkdtemp(prefix="sipplauncher_test_Test_chdir_")
     sipplauncher.utils.Utils.gen_file_struct(dirpath, mock_fs)
 
-#    mocker.patch('sipplauncher.PysippProcess.PysippProcess.run', new=lambda x: sys.exit(0))
     logging.getLogger("pysipp").propagate = 0
 
     test = SIPpTest(os.path.join(dirpath, TEST_NAME))
@@ -76,7 +68,6 @@ def test(mocker, mock_fs, args, expected):
     assert(test.state == STATES[1])
 
     test.run(TEST_RUN_ID, parsed_args)
-#    assert(test.state == STATES[5])
 
     def check_folder(fs_path, root):
         for a, b in iter(root.items()):
