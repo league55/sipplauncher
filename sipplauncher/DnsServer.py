85e4b799f9f93bec62dbf4eca5e1e19005f6bdda
Andrii Arsirii <pcapdump@gmail.com>
2019-12-26 10:19:34 -0500

    fix

--- sipplauncher/DnsServer.py
+++ sipplauncher/DnsServer.py
@@ -23,6 +23,7 @@ The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 """
 
+import logging
 from dnslib.server import (DNSServer,
                            BaseResolver)
 from dnslib import (DNSLabel,
