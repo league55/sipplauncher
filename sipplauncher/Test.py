046077d65f51217f686d340971652a05efaf33d7
Andrii Arsirii <pcapdump@gmail.com>
2019-12-26 04:25:28 -0500

    upd logging

--- sipplauncher/Test.py
+++ sipplauncher/Test.py
@@ -151,7 +151,7 @@ class SIPpTest(object):
                 raise SIPpTest.ScriptRunException(script + " returned code " + str(ret))
 
     def __get_logger(self):
-        return logging.getLogger(__name__ + "." + self.run_id)
+        return logging.getLogger(self.run_id)
 
     def __create_temp_folder(self):
         self.__temp_folder = os.path.join(DEFAULT_TEMP_FOLDER, self.key, self.run_id)
@@ -160,7 +160,7 @@ class SIPpTest(object):
 
     def __init_logger(self):
         # get base logger instance according to options from config
-        l_base = logging.getLogger(__name__)
+        l_base = logging.getLogger()
         # get child logger
         l = self.__get_logger()
         # Check if base logger has DynamicFileHandler specified
