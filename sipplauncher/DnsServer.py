43241f17d4f0253ffc80d85ce6900e51d1ead2be
Andrii Arsirii <pcapdump@gmail.com>
2019-12-26 10:59:28 -0500

    fix logging

--- sipplauncher/DnsServer.py
+++ sipplauncher/DnsServer.py
@@ -93,7 +93,7 @@ class Resolver(BaseResolver):
     @staticmethod
     def __load(file, run_id):
         assert(os.path.exists(file))
-        logging.getLogger(run_id).info('loading DNS file {0}'.format(file))
+        logging.getLogger("sipplauncher.Test." + run_id).info('loading DNS file {0}'.format(file))
         records = []
         with open(file, 'r') as f:
             for line in f:
@@ -111,11 +111,11 @@ class Resolver(BaseResolver):
 
                     record = Record(rname, rtype, args)
                     records.append(record)
-                    logging.getLogger(run_id).info(' %2d: %s', len(records), record)
+                    logging.getLogger("sipplauncher.Test." + run_id).info(' %2d: %s', len(records), record)
                 except Exception as e:
                     raise RuntimeError('Error processing line ({0}: {1}) "{2}"'.format(e.__class__.__name__), e, line.strip()) from e
 
-        logging.getLogger(run_id).info('%d zone resource records generated from file', len(records))
+        logging.getLogger("sipplauncher.Test." + run_id).info('%d zone resource records generated from file', len(records))
         return records
 
     def resolve(self, request, handler):
@@ -126,7 +126,7 @@ class Resolver(BaseResolver):
             for record in records:
                 if record.match(request.q):
                     reply.add_answer(record.rr)
-                    logging.getLogger(run_id).info('found zone for {0}[{1}]'.format(request.q.qname, type_name))
+                    logging.getLogger("sipplauncher.Test." + run_id).info('found zone for {0}[{1}]'.format(request.q.qname, type_name))
 
         if reply.rr:
             return reply
@@ -136,7 +136,7 @@ class Resolver(BaseResolver):
             for record in records:
                 if record.sub_match(request.q):
                     reply.add_answer(record.rr)
-                    logging.getLogger(run_id).info('found higher level SOA resource for {0}[{1}]'.format(request.q.qname, type_name))
+                    logging.getLogger("sipplauncher.Test." + run_id).info('found higher level SOA resource for {0}[{1}]'.format(request.q.qname, type_name))
 
         if reply.rr:
             return reply
