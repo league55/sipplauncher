d01c5f4843255a600230abc9f9a84e62adaa1bcd
Andrii Arsirii <pcapdump@gmail.com>
2020-01-06 06:31:21 -0500

    simpliy

--- sipplauncher/Test.py
+++ sipplauncher/Test.py
@@ -413,8 +413,6 @@ class SIPpTest(object):
                 elapsed_str=' - took %.0fs' % (elapsed)
                 self.__print_run_state(run_id_prefix, extra=elapsed_str)
                 if raise_exception:
-                    # We should propagate exception to the caller if it's caused by internal error.
-                    # This stops tests execution.
                     raise raise_exception
             else:
                 self.__set_state(SIPpTest.State.CLEAN)
