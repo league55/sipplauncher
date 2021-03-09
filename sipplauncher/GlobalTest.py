f149e09d2e79b2a9391164c24d079fb850c6588d
Nacho Garcia Segovia <nacho.gs@zaleos.net>
2021-03-09 17:59:32 +0100

    Fix Global Scenarios

--- sipplauncher/GlobalTest.py
+++ sipplauncher/GlobalTest.py
@@ -43,6 +43,7 @@ class GlobalTest(SIPpTest):
         """
         start = time.time()
         self.run_id = sipplauncher.utils.Utils.generate_id(n=6, just_letters=True)
+        self.run_id_number = sipplauncher.utils.Utils.generate_id(n=12, just_digits=True)
         self._set_state(SIPpTest.State.PREPARING)
         self._print_run_state(run_id_prefix)
         self._create_temp_folder()
