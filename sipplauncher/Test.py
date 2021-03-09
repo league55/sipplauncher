fc7f0f2d8b07edd06ffa02b42adad9a2d53105af
Daniel Martínez Ruiz <d.martinez@zaleos.net>
2021-03-09 11:50:33 +0100

    Reverted last changes

--- sipplauncher/Test.py
+++ sipplauncher/Test.py
@@ -191,12 +191,6 @@ class SIPpTest(object):
                 handler.set_folder(self.__temp_folder)
                 l.addHandler(handler)
 
-    def _generate_number(self, the_string):
-        ret = ""
-        for character in the_string:
-            ret += "%02d" % (ord(character) - ord("a"))
-        return ret
-
     def _replace_keywords(self, args):
         """ Loops over files in temp folder and replaces keywords in files.
 
@@ -212,7 +206,6 @@ class SIPpTest(object):
             "test": {
                 "name": self.key,
                 "run_id": self.run_id,
-                "run_id_number": self._generate_number(self.run_id),
             },
             "custom_transport": "", # TODO: remove this
         }
